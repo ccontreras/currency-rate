c88f1fefaa66e04933ca6264832d790311421324
Cesar Contreras <cesar.contreras@ikeasdg.local>
2016-08-08 12:16:29 -0400

    Changed namespace

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <?php
 
-// require_once 'src/CurrencyConverter.php';
-//
-// echo ($value = \linom\CurrencyConverter::convert('USD', 'DOP', 1)) !== null ? number_format($value, 2, '.', ',') : 'No data';
+require_once 'src/CurrencyConverter.php';
 
-echo '<pre>';
-print_r(printer_list(PRINTER_ENUM_LOCAL));
-exit;
-
-// printer_open();
-
-// phpinfo();
+echo ($value = \linom\CurrencyConverter::convert('USD', 'DOP', 1)) !== null ? number_format($value, 2, '.', ',') : 'No data';
