5e9f000e7d58047739b3864091a27bb06463b4ca
Cesar Contreras <cesar.contreras@ikeasdg.local>
2016-08-08 12:14:16 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+// require_once 'src/CurrencyConverter.php';
+//
+// echo ($value = \linom\CurrencyConverter::convert('USD', 'DOP', 1)) !== null ? number_format($value, 2, '.', ',') : 'No data';
+
+echo '<pre>';
+print_r(printer_list(PRINTER_ENUM_LOCAL));
+exit;
+
+// printer_open();
+
+// phpinfo();
---
+++ src/CurrencyConverter.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace linom;
+
+/**
+ * Convert currency to any valid symbol
+ *
+ * @author Cesar Contreras <ccdl15c@gmail.com>
+ * @package linom
+ * @version v0.0.1-alpha
+ */
+class CurrencyConverter {
+
+    /** @var string The converter service endpoint. */
+    private static $endpoint = 'http://www.google.com/finance/converter';
+
+    public static $error;
+
+    /**
+     * Convert currencies.
+     *
+     * @param    string        $from      From currency.
+     * @param    string        $to        To currency.
+     * @param    double|int    $amount    The amount to be converted.
+     * @return   double
+     */
+    public static function convert($from, $to, $amount) {
+        $amount = urlencode($amount);
+        $from = urlencode($from);
+        $to = urlencode($to);
+
+        $url = static::$endpoint."?a=$amount&from=$from&to=$to";
+
+        $ch = curl_init($url);
+
+        curl_setopt($ch, CURLOPT_HTTPGET, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $rawData = curl_exec($ch);
+
+        curl_close($ch);
+
+        if (preg_match('/bld>([\d\.]+)/', $rawData, $matches)) {
+            return round($matches[1], 2);
+        }
+
+        return null;
+    }
+
+    private static function debug($data, $exit = true) {
+        echo '<pre>';
+        print_r($data);
+
+        if ($exit) {
+            exit;
+        }
+    }
+
+}
